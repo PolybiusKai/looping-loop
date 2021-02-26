84a80a8dadfc67cad830b72d7ae2eb81dcecbef7
rsmith <legendary.belief@gmail.com>
2021-02-26 12:45:07 -0800

    Done.

--- looping.rb
+++ looping.rb
@@ -1,7 +1,8 @@
 def looping
 	#your code here
 	loop do
-		puts "I have foudn the Time Machine!" 
+		#puts "I have foudn the Time Machine!" 
+		puts " Never pass a school bus if it has flashing red lights"
 	end
 end
 
